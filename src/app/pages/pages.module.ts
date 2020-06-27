99b030dc0fd09d6a3ee55b0504f3f8a3f0962353
Orlando Franco Cespedes <orlandof@digitalware.com.co>
2020-06-27 16:11:42 -0500

    Sección 8

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -13,6 +13,8 @@ import { PAGES_ROUTES } from './pages.routes';
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficaDonaComponent } from '../components/grafica-dona/grafica-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     declarations: [
@@ -22,7 +24,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         Graficas1Component,
         IncrementadorComponent,
         GraficaDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         DashboardComponent,
---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: []
+})
+export class RxjsComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+
+  constructor() {
+    this.subscription = this.regresaObservable().pipe(
+      // retry(2) // reintentar 2 veces
+    )
+    .subscribe(
+      numero => console.log('Subs ', numero),
+      error => console.error('Error: ', error),
+      () => console.log('El observador termonó')
+    );
+   }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  regresaObservable(): Observable<any> {
+    return new Observable( (observable: Subscriber<any>) => {
+      let contador = 0;
+      const intervalo = setInterval( () => {
+        contador ++;
+
+        const salida = {
+          valor: contador
+        };
+
+        observable.next( salida );
+
+        // if ( contador === 3) {
+        //   clearInterval( intervalo );
+        //   observable.complete();
+        // }
+
+        // if ( contador === 2 ) {
+        //   // clearInterval( intervalo );
+        //   observable.error('Auxilio');
+        // }
+
+      }, 1000);
+    }).pipe(
+      map( resp => resp.valor ), // transformar respuesta
+      filter( ( valor, index ) => {
+        // filtar solo números pares
+        if ( (valor % 2) === 1 ) {
+          // impar
+          return true;
+        } else {
+          // par
+          return false;
+        }
+      })
+    );
+  }
+
+}
